1d5001e9406f2b957aa2929ddf07c31a3d62176a
孑愁 <401828628@qq.com>
2023-11-10 16:16:08 +0800

    Android 广告支持全屏展示

--- android/src/main/java/com/maochunjie/csjsdk/SplashAd.kt
+++ android/src/main/java/com/maochunjie/csjsdk/SplashAd.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Build
 import android.view.View
+import android.view.WindowManager
 import com.bytedance.sdk.openadsdk.AdSlot
 import java.lang.ref.WeakReference
 
@@ -24,6 +25,7 @@ object SplashAd {
         if (!activity.isFinishing) {
           mSplashDialog = Dialog(activity, resourceId)
           mSplashDialog!!.setContentView(splashView)
+          setActivityAndroidP(mSplashDialog);
           mSplashDialog!!.setCancelable(false)
           if (!mSplashDialog!!.isShowing) {
             mSplashDialog!!.show()
@@ -75,6 +77,19 @@ object SplashAd {
       (dp * density + 0.5f).toInt()
     } catch (e: Exception) {
       dp.toInt()
+    }
+  }
+
+  private fun setActivityAndroidP(dialog: Dialog?) {
+    //设置全屏展示
+    if (Build.VERSION.SDK_INT >= 28) {
+      if (dialog != null && dialog.window != null) {
+        dialog.window!!.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS) //全屏显示
+        val lp = dialog.window!!.attributes
+        lp.layoutInDisplayCutoutMode =
+          WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+        dialog.window!!.attributes = lp
+      }
     }
   }
 }
